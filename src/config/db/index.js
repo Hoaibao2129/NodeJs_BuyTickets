8c5392123c091995de774ab64643481fbfa8a6c0
HoaiBaoNguyen <hoaibao2129@gmail.com>
2022-12-30 15:16:30 +0700

    c1 all feature

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/BuyTicket_Dev');
+    await mongoose.connect('mongodb://127.0.0.1:27017/BuyTickets_Dev');
     console.log('Connect Successfully !');
   } catch (error) {
     console.log('Connect Fail !');
